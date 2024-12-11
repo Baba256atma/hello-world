29671075e8c50a9131bfecf0e6c65981c9489553
BAHADOR <bahaabti@gmail.com>
2024-12-10 16:46:56 -0800

    test , templates done

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,16 @@
 # pages/urls.py
 from django.urls import path
 from . import views
+
+"""
 urlpatterns = [
     path('', views.homePageView, name='home')
 ]
+"""
+urlpatterns = [
+    path("", views.HomePageView.as_view(), name="home"),
+    path("about/", views.AboutPageView.as_view(), name="about"),
+
+]
+      
+
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
-# Create your views here.
+## Create your views here.
+## pages/views.py
+#from django.http import HttpResponse
+#def homePageView(request):
+#    return HttpResponse('Hello, World!')
+
+
 # pages/views.py
-from django.http import HttpResponse
-def homePageView(request):
-    return HttpResponse('Hello, World!')
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView): 
+    template_name = "home.html"
+
+
+class AboutPageView(TemplateView): 
+    template_name = "about.html"
